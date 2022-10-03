7f6539fa7e3667470666b6452abce487cc2b421c
Fluxticks <benjigarment.appdev@gmail.com>
2022-10-04 00:29:38 +0100

    Added Auth attributes and __slots__

--- Auth.py
+++ Auth.py
@@ -1,3 +1,7 @@
 class Auth:
-    def __init__(self):
-        pass
+    __slots__ = ("_project_id", "_access_token", "_refresh_token")
+
+    def __init__(self, project_id: str, access_token: str, refresh_token: str):
+        self._project_id = project_id
+        self._access_token = access_token
+        self._refresh_token = refresh_token
