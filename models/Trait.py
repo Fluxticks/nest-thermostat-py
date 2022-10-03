0417836cad535597e23da7cf49b74b6cd7d534fb
Fluxticks <benjigarment.appdev@gmail.com>
2022-10-03 21:06:29 +0100

    Added trait init

--- models/Trait.py
+++ models/Trait.py
@@ -1,3 +1,6 @@
 class Trait:
 
-    pass
+    def __init__(self, domain: str, value: [str, dict]):
+        self._domain = domain
+        self.value = value
+        self.name = self._domain.split('.')[-1]
