33c283878b561b892ac16f5c79328dcac0086ddd
Fluxticks <benjigarment.appdev@gmail.com>
2022-10-03 21:45:13 +0100

    Added ConnectivityTrait subclass

--- models/Trait.py
+++ models/Trait.py
@@ -1,6 +1,36 @@
-class Trait:
+from abc import abstractclassmethod
 
-    def __init__(self, domain: str, value: [str, dict]):
+
+class Trait:
+    __slots__ = ("_domain", "name")
+
+    def __init__(self, domain: str):
         self._domain = domain
-        self.value = value
-        self.name = self._domain.split('.')[-1]
+        self.name = self._domain.split(".")[-1]
+
+    @abstractclassmethod
+    def domain(cls):
+        pass
+
+
+class ConnectivityTrait(Trait):
+    __slots__ = "_status"
+
+    def __init__(self, data):
+        if not data:
+            self._status = "OFFLINE"
+        else:
+            self._status = data.get("status")
+        super().__init__(self.domain)
+
+    @classmethod
+    def domain(cls):
+        return "sdm.devices.traits.Connectivity"
+
+    @property
+    def status(self):
+        return self._status
+
+    @property
+    def is_connected(self):
+        return self._status == "ONLINE"
