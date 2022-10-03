2c6e0022b67ce4cbf65e7763f66decd7150ce10e
Fluxticks <benjigarment.appdev@gmail.com>
2022-10-03 21:52:53 +0100

    Added FanTrait subclass

--- models/Trait.py
+++ models/Trait.py
@@ -16,12 +16,12 @@ class Trait:
 class ConnectivityTrait(Trait):
     __slots__ = "_status"
 
-    def __init__(self, data):
+    def __init__(self, data: dict):
         if not data:
             self._status = "OFFLINE"
         else:
             self._status = data.get("status")
-        super().__init__(self.domain)
+        super().__init__(self.domain())
 
     @classmethod
     def domain(cls):
@@ -34,3 +34,33 @@ class ConnectivityTrait(Trait):
     @property
     def is_connected(self):
         return self._status == "ONLINE"
+
+
+class FanTrait(Trait):
+    __slots__ = ("_timer_mode", "_timer_timeout")
+
+    def __init__(self, data: dict):
+        if not data:
+            self._timer_mode = None
+            self._timer_timeout = None
+        else:
+            self._timer_mode = data.get("timerMode")
+            self._timer_timeout = data.get("timerTimeout")
+        super().__init__(self.domain())
+
+    @classmethod
+    def domain(cls):
+        return "sdm.devices.traits.Fan"
+
+    @property
+    def mode(self):
+        return self._timer_mode
+
+    @property
+    def is_on(self):
+        return self.mode == "ON"
+
+    @property
+    def stop_time(self):
+        # TODO: Convert to datetime value
+        return self._timer_timeout
