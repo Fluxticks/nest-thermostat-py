4c5a55394350dcf30ed0e50b8961d62f11b9d5f7
Fluxticks <benjigarment.appdev@gmail.com>
2022-10-04 02:10:39 +0100

    Removed unnecessary properties from Thermostat and moved to Trait model

--- models/Thermostat.py
+++ models/Thermostat.py
@@ -22,16 +22,6 @@ class Thermostat:
     __slots__ = (
         "_raw_data",
         "_device_id",
-        "_connectivity",
-        "_fan",
-        "_humidity",
-        "_info",
-        "_settings",
-        "_temperature",
-        "_thermostat_eco",
-        "_thermostat_hvac",
-        "_thermostat_mode",
-        "_thermostat_temperature",
         *__traits__,
     )
 
@@ -53,30 +43,10 @@ class Thermostat:
         self._device_id = data.get("name", "").split("/")[-1]
 
         traits = data.get("traits")
-        self._connectivity = ConnectivityTrait(traits.get(ConnectivityTrait.domain()))
-        self._fan = FanTrait(traits.get(FanTrait.domain()))
-        self._humidity = HumidityTrait(traits.get(HumidityTrait.domain()))
-        self._info = InfoTrait(traits.get(InfoTrait.domain()))
-        self._settings = SettingsTrait(traits.get(SettingsTrait.domain()))
-        self._temperature = TemperatureTrait(traits.get(TemperatureTrait.domain()))
-        self._thermostat_eco = ThermostatEcoTrait(
-            traits.get(ThermostatEcoTrait.domain())
-        )
-        self._thermostat_hvac = ThermostatHvacTrait(
-            traits.get(ThermostatHvacTrait.domain())
-        )
-        self._thermostat_mode = ThermostatModeTrait(
-            traits.get(ThermostatModeTrait.domain())
-        )
-        self._thermostat_temperature = ThermostatTemperatureSetpointTrait(
-            traits.get(ThermostatTemperatureSetpointTrait.domain())
-        )
 
-        traits = self.__slots__[2:-10]
-
-        for trait in traits:
-            attr = getattr(self, trait)
-            setattr(self, attr.__class__.__name__.replace("Trait", ""), attr)
+        for trait in self.__traits__:
+            cls = globals()[f"{trait}Trait"]
+            setattr(self, trait, cls(traits.get(cls.domain())))
 
     async def update_device(self, api: Api):
         data = await api.get_exact_device(self.device_id)
@@ -85,59 +55,3 @@ class Thermostat:
     @property
     def device_id(self):
         return self._device_id
-
-    @property
-    def is_connected(self):
-        return self._connectivity.is_connected
-
-    @property
-    def is_fan_on(self):
-        return self._fan.is_on
-
-    @property
-    def fan_timer_timeout(self):
-        return self._fan.stop_time
-
-    @property
-    def humidity(self):
-        return self._humidity.humidity
-
-    @property
-    def device_name(self):
-        return self._info.name
-
-    @property
-    def temperature_unit(self):
-        return self._settings.temperature_unit
-
-    @property
-    def room_temperature(self):
-        return self._temperature.room_temperature
-
-    @property
-    def eco_mode_on(self):
-        return self._thermostat_eco.eco_mode_on
-
-    @property
-    def eco_heat_target(self):
-        return self._thermostat_eco.heat_target
-
-    @property
-    def eco_cool_target(self):
-        return self._thermostat_eco.cool_target
-
-    @property
-    def hvac_mode_on(self):
-        return self._thermostat_hvac.is_on
-
-    @property
-    def thermostat_mode(self):
-        return self._thermostat_mode.mode
-
-    @property
-    def thermostat_heat_target(self):
-        return self._thermostat_temperature.heat_temperature
-
-    @property
-    def thermostat_cool_target(self):
-        return self._thermostat_temperature.cool_temperature
