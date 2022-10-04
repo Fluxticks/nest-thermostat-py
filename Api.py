d0f554d35d2e99f044526b1c0804c27679260482
Fluxticks <benjigarment.appdev@gmail.com>
2022-10-04 01:06:25 +0100

    Added Specific Device endpoint

--- Api.py
+++ Api.py
@@ -24,3 +24,19 @@ class Api:
                     )
                 else:
                     return await resp.json().get("devices")
+
+    async def get_exact_device(self, device_id: str):
+        session_token = await self._auth.get_access_token()
+        DEVICE_URL = f"https://smartdevicemanagement.googleapis.com/v1/enterprises/{self._auth.project_id}/devices/{device_id}"
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {session_token}",
+        }
+        async with aiohttp.ClientSession() as session:
+            async with session.get(DEVICE_URL, headers) as resp:
+                if resp.status != 200:
+                    raise ValueError(
+                        f"Unable to access device with ID {device_id}. Could be incorrect credentials or device ID"
+                    )
+                else:
+                    return await resp.json()
